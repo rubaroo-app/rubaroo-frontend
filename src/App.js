fe148368bd9baddf885812883a0ceae6cc56a732
Raj <raj.rahul.singh@gmail.com>
2026-06-25 18:20:04 +0530

    feat: yellow landing page as home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+const LandingRedirect = () => {
+  useEffect(() => {
+    window.location.href = "/landing.html";
+  }, []);
+  return null;
+};
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -17,7 +24,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/register" />} />
+        <Route path="/" element={<LandingRedirect />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/otp-verify" element={<OtpVerify />} />
